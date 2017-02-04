ff749c87126d5d96886989d857e64eb32bc3d1da
Zhengyang Zhao <zyzhao@thoughtworks.com>
2017-02-05 00:12:22 +0800

    fix travis

---
+++ src/test/java/com/pair/HelloControllerTest.java
@@ -0,0 +1,15 @@
+package com.pair;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by zyzhao on 2/5/17.
+ */
+public class HelloControllerTest {
+    @Test
+    public void testHello() throws Exception {
+
+    }
+}
