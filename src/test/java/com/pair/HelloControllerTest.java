0ffd7c17f6403636d4fc576887420bdc332936d3
Zhengyang Zhao <zyzhao@thoughtworks.com>
2017-02-05 00:32:24 +0800

    fix travis

--- src/test/java/com/pair/HelloControllerTest.java
+++ src/test/java/com/pair/HelloControllerTest.java
@@ -1,13 +1,21 @@
 package com.pair;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.Assert.*;
 
 /**
  * Created by zyzhao on 2/5/17.
  */
+@RunWith(SpringRunner.class)
 public class HelloControllerTest {
+
+    private MockMvc mockMvc;
+
     @Test
     public void testHello() throws Exception {
 
